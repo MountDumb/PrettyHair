dc1f74ab1fce92dd487a0ba5fef985190c94753d
UrbanSkurk <UrbanSkurk@VARMEVRK>
2016-11-15 10:13:58 +0100

    Add project files.

---
+++ PrettyHair.Lib/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrettyHair.Lib
+{
+    public class Product
+    {
+        #region Fields
+        private int _productId;
+        private decimal _price;
+        private string _description;
+        #endregion
+
+        #region Properties
+        public int ProductId
+        {
+            get { return _productId; }
+            set { _productId = value; }
+        }
+        public decimal Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+
+        #endregion
+        public Product(int productId, decimal price, string description)
+        {
+            _productId = productId;
+            _price = price;
+            _description = description;
+        }
+    }
+}
---
+++ PrettyHair.Lib/ProductRepo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrettyHair.Lib
+{
+    public class ProductRepo
+    {
+        #region Fields
+        
+        private List<Product> _productCol;
+        #endregion
+
+        public ProductRepo()
+        {
+            _productCol = new List<Product>();
+        }
+
+        public IList<Product> GetProducts()
+        {
+            return _productCol;
+        }
+
+        public Product GetProductById(int id)
+        {
+            return _productCol.Find(x => x.ProductId == id);
+        } 
+
+       
+
+        public void DeleteProductById(int id)
+        {
+            _productCol.Remove(_productCol.Find(x => x.ProductId == id));
+        }
+
+        public void UpdateProduct(Product p)
+        {
+            //productCol.Find(x => x.ProductId == p.ProductId).Price = p.Price;
+            //_productCol.Find(x => x.ProductId == p.ProductId).Description = p.Description;
+            int i = _productCol.FindIndex(x => x.ProductId == p.ProductId);
+            _productCol[i] = p;
+        }
+   }
+}
